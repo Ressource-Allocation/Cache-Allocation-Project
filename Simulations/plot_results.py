835d45441e33c2161b51f95f63754062bc892368
challenger009 <ayoub.benameur@supcom.tn>
2020-10-21 15:21:13 +0200

    deleting old files

--- Simulations/plot_results.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-df = pd.read_csv('sarsa_cache1000_request_rate10_nb_interval40000interval_size1delta20.csv', header=1)
-plt.plot(df['Time_hours'],df['Total_Cost'])
