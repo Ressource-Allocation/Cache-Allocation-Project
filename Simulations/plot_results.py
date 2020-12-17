d2c1b91cbaf046c72ef81e6c1c2cb4ed66840630
Ayoub Ben Ameur <ayoubbenameur@MacBook-Pro-de-Ayoub.local>
2020-12-17 17:22:57 +0100

    update plotting file

--- Simulations/plot_results.py
+++ Simulations/plot_results.py
@@ -11,7 +11,7 @@ plt.rcParams.update({
 plt.style.use('classic')
 
 #Nominal cost dynamic delta 
-df3 = pd.read_csv('sarsa_cache1000_request_rate1000_nb_interval40000interval_size10delta10method_Q_learning_dynamic.csv', header=0)
+df3 = pd.read_csv('sarsa_cache1000_request_rate1000_nb_interval40000interval_size1delta10method_Q_learningcoeffcients_[1, 2, 4, 8, 16].csv', header=0)
 
 plt.figure()
 df3['Nominal_Cost'].rolling(window=600).mean().plot()
@@ -19,7 +19,7 @@ df3['Best_Cost'].rolling(window=600).mean().plot()
 df3['Cost_First'].rolling(window=600).mean().plot()
 df3['Total_Cost'].rolling(window=600).mean().plot()
 plt.legend(['Nominal_Cost', 'Best_Cost','Cost_First','Total_Cost'])
-plt.title("Q-Learning with request_rate = 1000, interval size = 10, and delta = 10, 20, 40")
+plt.title("Q-Learning with request_rate = 1000, interval size = 1, and delta = 10, 20, 40, 80, 160")
 plt.xlabel("Time")
 
 plt.show()
